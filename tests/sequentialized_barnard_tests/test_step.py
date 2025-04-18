9e71d7fce24b7285543dd303907ccbd940a29a83
DavidSnyder-TRI <david.snyder.pi@tri.global>
2025-04-18 13:51:27 -0400

    Incorporate CleanUpSpill data into pytest for STEP. Need to visualize policy

--- tests/sequentialized_barnard_tests/test_step.py
+++ tests/sequentialized_barnard_tests/test_step.py
@@ -1,5 +1,8 @@
 """Unit tests for the STEP procedure"""
 
+import os
+from pathlib import Path
+
 import numpy as np
 import pytest
 
@@ -7,6 +10,15 @@ from sequentialized_barnard_tests import Decision, Hypothesis
 from sequentialized_barnard_tests.step import MirroredStepTest, StepTest
 
 ##### STEP Test #####
+paper_data_path = str(
+    Path(
+        os.path.join(
+            os.path.dirname(os.path.abspath(__file__)),
+            "../../sequentialized_barnard_tests/eval_data/",
+        )
+    ).resolve()
+)
+eval_trajectories = np.load(f"{paper_data_path}/TRI_CLEAN_SPILL_v2.npy")
 
 
 @pytest.fixture(scope="module")
@@ -51,6 +63,10 @@ def test_step_input_value_error(step):
         (Hypothesis.P0MoreThanP1, np.zeros(15), np.ones(15), Decision.FailToDecide),
         (Hypothesis.P0LessThanP1, np.ones(15), np.zeros(15), Decision.FailToDecide),
         (Hypothesis.P0MoreThanP1, np.ones(15), np.zeros(15), Decision.AcceptAlternative),
+        (Hypothesis.P0LessThanP1, eval_trajectories[:, 1], eval_trajectories[:, 0], Decision.AcceptAlternative),
+        (Hypothesis.P0MoreThanP1, eval_trajectories[:, 1], eval_trajectories[:, 0], Decision.FailToDecide),
+        (Hypothesis.P0LessThanP1, eval_trajectories[:, 0], eval_trajectories[:, 1], Decision.FailToDecide),
+        (Hypothesis.P0MoreThanP1, eval_trajectories[:, 0], eval_trajectories[:, 1], Decision.AcceptAlternative),
         # fmt: on
     ],
     indirect=["step"],
@@ -58,6 +74,51 @@ def test_step_input_value_error(step):
 def test_step(step, sequence_0, sequence_1, expected):
     result = step.run_on_sequence(sequence_0, sequence_1)
     assert result.decision == expected
+
+
+@pytest.mark.parametrize(
+    ("step", "sequence_0", "sequence_1", "expected"),
+    [
+        # fmt: off
+        (Hypothesis.P0LessThanP1, eval_trajectories[:, 1], eval_trajectories[:, 0], 22.5),
+        (Hypothesis.P0MoreThanP1, eval_trajectories[:, 1], eval_trajectories[:, 0], 50),
+        (Hypothesis.P0LessThanP1, eval_trajectories[:, 0], eval_trajectories[:, 1], 50),
+        (Hypothesis.P0MoreThanP1, eval_trajectories[:, 0], eval_trajectories[:, 1], 22.5),
+        # fmt: on
+    ],
+    indirect=["step"],
+)
+def test_step_time(step, sequence_0, sequence_1, expected):
+    result = step.run_on_sequence(sequence_0, sequence_1)
+    assert np.abs(float(result.info["Time"]) - expected) <= 3.0
+
+
+@pytest.fixture(scope="module")
+def step500(request):
+    test = StepTest(
+        alternative=request.param,
+        n_max=500,
+        alpha=0.05,
+    )
+
+    return test
+
+
+@pytest.mark.parametrize(
+    ("step500", "sequence_0", "sequence_1", "expected"),
+    [
+        # fmt: off
+        (Hypothesis.P0LessThanP1, eval_trajectories[:, 1], eval_trajectories[:, 0], 33),
+        (Hypothesis.P0MoreThanP1, eval_trajectories[:, 1], eval_trajectories[:, 0], 50),
+        (Hypothesis.P0LessThanP1, eval_trajectories[:, 0], eval_trajectories[:, 1], 50),
+        (Hypothesis.P0MoreThanP1, eval_trajectories[:, 0], eval_trajectories[:, 1], 33),
+        # fmt: on
+    ],
+    indirect=["step500"],
+)
+def test_step500_time(step500, sequence_0, sequence_1, expected):
+    result = step500.run_on_sequence(sequence_0, sequence_1)
+    assert np.abs(result.info["Time"] - expected) <= 1.5
 
 
 ##### Mirrored STEP Test #####
@@ -93,3 +154,48 @@ def mirrored_step(request):
 def test_mirrored_step(mirrored_step, sequence_0, sequence_1, expected):
     result = mirrored_step.run_on_sequence(sequence_0, sequence_1)
     assert result.decision == expected
+
+
+@pytest.mark.parametrize(
+    ("mirrored_step", "sequence_0", "sequence_1", "expected"),
+    [
+        # fmt: off
+        (Hypothesis.P0LessThanP1, eval_trajectories[:, 1], eval_trajectories[:, 0], 25),
+        (Hypothesis.P0MoreThanP1, eval_trajectories[:, 1], eval_trajectories[:, 0], 25),
+        (Hypothesis.P0LessThanP1, eval_trajectories[:, 0], eval_trajectories[:, 1], 25),
+        (Hypothesis.P0MoreThanP1, eval_trajectories[:, 0], eval_trajectories[:, 1], 25),
+        # fmt: on
+    ],
+    indirect=["mirrored_step"],
+)
+def test_mirrored_step_time(mirrored_step, sequence_0, sequence_1, expected):
+    result = mirrored_step.run_on_sequence(sequence_0, sequence_1)
+    assert np.abs(result.info["Time"] - expected) <= 1.5
+
+
+@pytest.fixture(scope="module")
+def mirrored_step500(request):
+    test = MirroredStepTest(
+        alternative=request.param,
+        n_max=500,
+        alpha=0.05,
+    )
+
+    return test
+
+
+@pytest.mark.parametrize(
+    ("mirrored_step500", "sequence_0", "sequence_1", "expected"),
+    [
+        # fmt: off
+        (Hypothesis.P0LessThanP1, eval_trajectories[:, 1], eval_trajectories[:, 0], 33),
+        (Hypothesis.P0MoreThanP1, eval_trajectories[:, 1], eval_trajectories[:, 0], 33),
+        (Hypothesis.P0LessThanP1, eval_trajectories[:, 0], eval_trajectories[:, 1], 33),
+        (Hypothesis.P0MoreThanP1, eval_trajectories[:, 0], eval_trajectories[:, 1], 33),
+        # fmt: on
+    ],
+    indirect=["mirrored_step500"],
+)
+def test_mirrored_step500_time(mirrored_step500, sequence_0, sequence_1, expected):
+    result = mirrored_step500.run_on_sequence(sequence_0, sequence_1)
+    assert np.abs(result.info["Time"] - expected) <= 1.5
